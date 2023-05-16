e737a10eccdaf796c6b911fc97c0a0a48f7eea4a
nnenna01 <nnennagere@yahoo.com>
2023-05-16 15:37:13 +0100

    This is an additional thing just for teaching

---
+++ src/test/java/stepDefs/Ukba_stepDefs.java
@@ -0,0 +1,26 @@
+package stepDefs;
+
+import io.cucumber.java.en.Given;
+
+public class Ukba_stepDefs extends baseutil{
+    this Base.base;
+
+
+
+    @Given("I am on the restful booker website")
+    public void i_am_on_the_restful_booker_website() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I Click on the homePage")
+    public void i_click_on_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I select {string} and {string}")
+    public void i_select_and(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
